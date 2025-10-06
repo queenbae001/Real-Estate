76dda1ac42bf7073f09bd3931fec1889c109d035
queenbae001 <queendera222@gmail.com>
2025-10-06 05:50:02 -0700

    orinal main

--- src/component/SecondH.jsx
+++ src/component/SecondH.jsx
@@ -7,12 +7,13 @@ const SecondH = () => {
     <div className="jara">
 <div className="ass">
             <h1>Properties For Sale</h1>
-            <p>Choose a good location (safe, accessible, with infrastructure).
+            <p>Choose a good location 
+              (safe, accessible, with infrastructure).
 
 Verify all property documents (C of O, Deed of Assignment, Survey Plan).</p>
         </div>
         <div className="ass2">
-            <h1>💡 Tips for Renting in Nigeria</h1>
+            <h1>Tips for Renting in Nigeria</h1>
             <p>Always inspect the property physically before making payments.</p>
         </div>
         <div className="ass3">
