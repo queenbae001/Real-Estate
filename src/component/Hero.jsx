6016fe8e259860cb9e33fa985dfe39a770dcf937
queenbae001 <queendera222@gmail.com>
2025-10-10 04:50:21 -0700

    Appoint

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -5,12 +5,12 @@ const Hero = () => {
     <div >
       <div className='cult'>
             <div className='soft'>
-      <h1>WELCOME to Queenie Real Estate</h1>
-      <h1>Your Trusted Partner in Property</h1>
-      <p> we make property ownership simple, secure, and satisfying. Whether you're buying,<br /> selling, renting, or investing, we offer tailored real estate solutions that <br /> meet your needs and exceed your expectations.</p>
+      <h1>WELCOME to Queenie Hotel</h1>
+      <h1> Your Trusted Partner in Comfort and Hospitality.</h1>
+      <p>We make your stay simple, secure, and satisfying. <br />Whether you’re here for business, leisure, or a longer visit,<br /> we offer personalized hospitality that meets your needs and exceeds your expectations.</p>
       </div>
       <div className='top'>
-        <button>Book Appointment</button>
+        <button>Book an Appointment</button>
         </div>
 
       </div>
